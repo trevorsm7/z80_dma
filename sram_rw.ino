9be125727fbdfd4186a914370c01f7ded6073abe
Trevor Smith <trevorsm7@gmail.com>
2021-02-15 20:31:32 -0800

    Update for Arduino Micro and dual 2114 chips

--- sram_rw.ino
+++ sram_rw.ino
@@ -1,42 +1,50 @@
-const byte LOW_MASK  = 0b00111111; // PORTB
-const byte HIGH_MASK = 0b00111100; // PORTD
-const byte CS_MASK   = 0b01000000; // PORTD
-const byte DATA_MASK = 0b00001111; // PORTC
-const byte WE_MASK   = 0b00010000; // PORTC
+const byte DATA_MASK = 0b11111111; // PORTB
+
+const byte ADDR_0_4_MASK = 0b00011111; // PORTD
+const byte CS_MASK = 0b10000000; // PORTD
+const byte WE_MASK = 0b01000000; // PORTD
+
+const byte ADDR_5_7_MASK = 0b11100000; // PORTF
+const byte ADDR_8_9_MASK = 0b00000011; // PORTF
+const byte ADDR_5_9_MASK = ADDR_5_7_MASK | ADDR_8_9_MASK;
+
+void write_address(uint16_t addr) {
+  const byte low = addr & 0xFF;
+  const byte high = addr >> 8;
+  PORTD = (PORTD & ~ADDR_0_4_MASK) | (low & ADDR_0_4_MASK);
+  PORTF = (PORTF & ~ADDR_5_9_MASK) | (low & ADDR_5_7_MASK) | (high & ADDR_8_9_MASK);
+}
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) {}
 
-  // Assign output pins
-  DDRB |= LOW_MASK;
-  DDRC |= WE_MASK;
-  DDRD |= HIGH_MASK | CS_MASK;
-
   // Set (disable) write enable and chip select
-  PORTC |= WE_MASK;
-  PORTD |= CS_MASK;
+  PORTD |= WE_MASK | CS_MASK;
+
+  // Assign output pins
+  DDRB = DATA_MASK; //DDRB |= DATA_MASK;
+  DDRD |= ADDR_0_4_MASK | WE_MASK | CS_MASK;
+  DDRF |= ADDR_5_9_MASK;
 }
 
 void memwrite(uint16_t addr, byte data) {
   // assign data pins as output
-  DDRC |= DATA_MASK;
+  DDRB = DATA_MASK; //DDRB |= DATA_MASK;
 
-  // write address
-  PORTD = (PORTD & ~HIGH_MASK) | ((addr >> 4) & HIGH_MASK);
-  PORTB = (PORTB & ~LOW_MASK) | (addr & LOW_MASK);
+  write_address(addr);
 
   // clear (enable) chip select
   PORTD &= ~CS_MASK;
 
   // clear (enable) write enable
-  PORTC &= ~WE_MASK; //__asm__("cbi 8,4\n\t");
+  PORTD &= ~WE_MASK; //__asm__("cbi ?,?\n\t");
 
-  //PORTC = (PORTC & ~(WE_MASK | DATA_MASK)) | (data & DATA_MASK);
-  PORTC = (PORTC & ~DATA_MASK) | (data & DATA_MASK);
+  PORTB = data; //PORTB = (PORTB & ~DATA_MASK) | (data & DATA_MASK);
+  // TODO need a nop?
 
   // disable write enable
-  PORTC |= WE_MASK; //__asm__("sbi 8,4\n\t");
+  PORTD |= WE_MASK; //__asm__("sbi ?,?\n\t");
 
   // set (disable) chip select
   PORTD |= CS_MASK;
@@ -44,19 +52,17 @@ void memwrite(uint16_t addr, byte data) {
 
 byte memread(uint16_t addr) {
   // assign data pins as input
-  PORTC &= ~DATA_MASK; //< disables pull-ups in input mode
-  DDRC &= ~DATA_MASK;
+  PORTB = 0;//~DATA_MASK; //PORTB &= ~DATA_MASK; //< disables pull-ups in input mode
+  DDRB = 0;//~DATA_MASK; //DDRB &= ~DATA_MASK;
 
-  // write address
-  PORTD = (PORTD & ~HIGH_MASK) | ((addr >> 4) & HIGH_MASK);
-  PORTB = (PORTB & ~LOW_MASK) | (addr & LOW_MASK);
+  write_address(addr);
 
   // clear (enable) chip select and wait 2 cycles (>70 ns)
   PORTD &= ~CS_MASK;
   __asm__("nop\n\t");
   __asm__("nop\n\t");
 
-  byte data = PINC & DATA_MASK;
+  const byte data = PINB;// & DATA_MASK;
 
   // set (disable) chip select
   PORTD |= CS_MASK;
@@ -72,39 +78,41 @@ void loop() {
       const int input = Serial.read();
       if (input >= 0) {
         memwrite(addr++, input); //< increment addr
-        memwrite(addr++, input >> 4); //< increment addr
         if (input == '\n')
           break;
       }
     }
   } else if (input == 'r') {
     // Stream external memory to Serial
-    for (uint16_t addr = 0; addr < 1024; addr += 2) {
-      const byte output = memread(addr + 1) << 4 | memread(addr);
+    for (uint16_t addr = 0; addr < 1024; ++addr) {
+      const byte output = memread(addr);
       Serial.write(output);
       if (output == '\n' || output == 0)
         break;
     }
   } else if (input == 't') {
-    const byte patterns[] = {0xF, 0x5, 0xA, 0x0};
+    bool fail = false;
+    const byte patterns[] = {0xFF, 0xF0, 0x0F, 0xCC, 0x33, 0xAA, 0x55, 0x00};
     for (byte pattern : patterns) {
       for (uint16_t addr = 0; addr < 1024; ++addr) {
-        memwrite(addr, (pattern ^ addr) & DATA_MASK);
+        memwrite(addr, pattern);
       }
       for (uint16_t addr = 0; addr < 1024; ++addr) {
         const byte data = memread(addr);
-        const byte expected = (pattern ^ addr) & DATA_MASK;
+        const byte expected = pattern;
         if (data != expected) {
           Serial.print("FAIL: ");
-          Serial.print(data, HEX);
+          Serial.print(data, BIN);
           Serial.print(" != ");
-          Serial.print(expected, HEX);
+          Serial.print(expected, BIN);
           Serial.print(" at ");
-          Serial.println(addr);
-          return;
+          Serial.println(addr, BIN);
+          fail = true;
         }
       }
     }
-    Serial.println("PASS");
+    if (!fail) {
+      Serial.println("PASS");
+    }
   }
 }
